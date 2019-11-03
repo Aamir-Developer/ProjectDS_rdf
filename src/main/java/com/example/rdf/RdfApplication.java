c766f1c65b7f23c7f61ed1e97f9fddb73199d2d0
Aamir Mohammed <maamir@mail.uni-paderborn.de>
2019-11-03 18:25:02 +0100

    OPAL data connected and fetched through java and fuseki

---
+++ src/main/java/com/example/rdf/RdfApplication.java
@@ -0,0 +1,15 @@
+package com.example.rdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdfApplication.class, args);
+		System.out.println("service started..");
+
+	}
+
+}
---
+++ src/test/java/com/example/rdf/RdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
